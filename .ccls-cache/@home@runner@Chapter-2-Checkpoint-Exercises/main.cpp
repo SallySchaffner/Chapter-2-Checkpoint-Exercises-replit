b77ea0e8e346fb5d7b84a356e3b3abc92ab1a1ff
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-06 23:35:22 +0000

    Code for Checkpoint 2.17 program

--- .ccls-cache/@home@runner@Chapter-2-Checkpoint-Exercises/main.cpp
+++ .ccls-cache/@home@runner@Chapter-2-Checkpoint-Exercises/main.cpp
@@ -1,5 +1,18 @@
+// This main function is for the Checkpoint 2.17 program
 #include <iostream>
+#include <string>
 
-int main() {
-  std::cout << "Hello World!\n";
+using namespace std;
+
+int main() 
+{
+  string name, address, phoneNumber;
+
+  name = "Sally Schaffner";
+  address = "243 Southlake Place\nNewport News VA 23602";
+  phoneNumber = "747-358-2457";
+
+  cout << name << endl << address << endl << phoneNumber << endl;
+
+  return 0;
 }
