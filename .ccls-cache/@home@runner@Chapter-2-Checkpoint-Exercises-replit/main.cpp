91691557fe28a28ed0e11ddb128ce197497d151b
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-06-16 03:20:02 +0000

    importing

---
+++ .ccls-cache/@home@runner@Chapter-2-Checkpoint-Exercises-replit/main.cpp
@@ -0,0 +1,14 @@
+// This main function is for the Checkpoint 2.20 program
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int age = 25;
+  float weight = 120;
+
+  cout << "My age is " << age << " and my weight is " << weight << " pounds." << endl;
+
+  return 0;
+}
